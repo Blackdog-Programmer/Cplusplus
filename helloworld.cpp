31d80519720f837ad2515723475ddcc89b58b951
blackdogcode <blackdogcode@gmail.com>
2019-10-22 23:36:21 +0900

    C++ Reference Image

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main(int argc, char *argv[]) {
-    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
+
+void cpp_strings() {
+    
+    string unformatted_full_name {"StephenHawking"};
+    
+    //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+    //----WRITE YOUR CODE BELOW THIS LINE----
+    string first_name(unformatted_full_name, 7);
+    string last_name = unformatted_full_name.substr(7, string::npos);
+    string formatted_full_name = first_name + " " + last_name;
+    //----WRITE YOUR CODE ABOVE THIS LINE----
+    //----DO NOT MODIFY THE CODE BELOW THIS LINE----
+    
+    cout << formatted_full_name;
 }
